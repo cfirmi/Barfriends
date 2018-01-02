fea10aeacbc35b0dbe25cfe67f449c7f8e5c8918
practice <practice@CPE003ee1c8f5f2-CM00fc8d41fe90.cpe.net.cable.rogers.com>
2018-01-02 15:42:53 -0500

    Desktop length

--- docs/js/navbar.js
+++ docs/js/navbar.js
@@ -24,4 +24,26 @@ function downloadLeave() {
 };
 
 
-})
+
+//Vibrate on Touch of button
+window.navigator = window.navigator || {};
+if (navigator.vibrate === undefined) {
+    document.getElementById('v-unsupported').classList.remove('hidden');
+    ['button-play-v-once', 'button-play-v-thrice', 'button-stop-v'].forEach(function(elementId) {
+      document.getElementById(elementId).setAttribute('disabled', 'disabled');
+    });
+      } else {
+          document.getElementById('button-play-v-one').addEventListener('click', function() {
+            navigator.vibrate(1000);
+            console.log('hello');
+          });
+          document.getElementById('button-play-v-second').addEventListener('click', function() {
+            navigator.vibrate([1000]);
+          });
+          document.getElementById('button-stop-v').addEventListener('click', function() {
+            navigator.vibrate(0);
+          });
+
+      }
+
+});
