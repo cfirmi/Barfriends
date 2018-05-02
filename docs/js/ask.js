756a95bd33634a18756a957a3e561d37655ed5ff
practice <practice@CPE003ee1c8f5f4-CM00fc8d41fe90.cpe.net.cable.rogers.com>
2018-05-02 12:40:26 -0400

    organized folder structures and cleaned up comp

--- docs/js/ask.js
+++ docs/js/ask.js
@@ -1,19 +1,19 @@
 $(".dance").hover(function () {
-    $(".ask-title").css({'background-image': 'url("img/DANCE-white.svg")', 'background-size': '1000px,1000px'});
+    $(".ask-title").css({'background-image': 'url("img/ASKMessages/messageDance.svg")', 'background-size': '1000px,1000px'});
     $("#features").css({'background': '#FF8700;'});
 });
 $(".message").hover(function () {
-    $(".ask-title").css({'background-image': 'url("img/anything.svg")', 'background-size': '1000px,1000px'});
+    $(".ask-title").css({'background-image': 'url("img/ASKMessages/messageMessage.svg")', 'background-size': '1000px,1000px'});
 });
 $(".drink").hover(function () {
-    $(".ask-title").css({'background-image': 'url("img/drink.svg")', 'background-size': '1000px,1000px'});
+    $(".ask-title").css({'background-image': 'url("img/ASKMessages/messageDrink.svg")', 'background-size': '1000px,1000px'});
 });
 
 
 
 $(document).ready(function(){
     var count = 0;
-    var images = ["img/iphone-x1.png","img/iphone-x2.png","img/iphone-x3.png", "img/iphone-x4.png", "img/iphone-x5.png", "img/iphone-x6.png"];
+    var images = ["img/barfriendsSamples/screenshot-1-A.png","img/barfriendsSamples/screenshot-2-A.png","img/barfriendsSamples/screenshot-3-A.png", "img/barfriendsSamples/screenshot-4-A.png", "img/barfriendsSamples/screenshot-5-A.png", "img/barfriendsSamples/screenshot-6-A.png"];
     var image = $(".fader");
 
 
